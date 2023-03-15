7fa15dcf099c37b86a3874a3908e90f538b87643
Pitelom18 <pitelom18@gmail.com>
2023-03-15 15:07:56 -0500

    se sube enlace de login en el index

--- JS/login.js
+++ JS/login.js
@@ -7,52 +7,44 @@ loginBtn.addEventListener('click', (event) => {
   const email = emailInput.value.trim();
   const password = passwordInput.value.trim();
 
-  
   if (!email || !isValidEmail(email)) {
-    
-    alert('Por favor ingrese un email valido');
+    alert('Por favor ingrese un email válido');
     return;
   }
 
   if (!password || password.length < 8) {
-    
-    alert('Por favor Ingrese una contraseña de minimo 8 caracteres');
+    alert('Por favor ingrese una contraseña de mínimo 8 caracteres');
     return;
   } else if (!isValidPassword(password)) {
-    
     const passwordStrength = getPasswordStrength(password);
-    alert(`Su contraseña es ${passwordStrength}`);
+    alert(`La fortaleza de su contraseña es ${passwordStrength}`);
     return;
-  }window.location.href = '/formularioAdmin.html';
-  
-
-  function getPasswordStrength(password) {
-    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
-    const length = password.length;
-    const complexity = passwordRegex.test(password) ? 2 : 1;
-    const strength = length * complexity;
-    if (strength < 5) {
-      return 'debil';
-    } else if (strength < 9) {
-      return 'media';
-    } else {
-      return 'fuerte';
-    }
   }
   
-  
-  
+  // Redirect the user to formularioAdmin.html
+  window.location.href = './formProductos.html';
 });
 
+function getPasswordStrength(password) {
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+  const length = password.length;
+  const complexity = passwordRegex.test(password) ? 2 : 1;
+  const strength = length * complexity;
+  if (strength < 5) {
+    return 'debil';
+  } else if (strength < 9) {
+    return 'media';
+  } else {
+    return 'fuerte';
+  }
+}
 
 function isValidEmail(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 }
 
-function isValidPassword(password){
-     const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
-     return passwordRegex.test(password);
+function isValidPassword(password) {
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+  return passwordRegex.test(password);
 }
-
-
