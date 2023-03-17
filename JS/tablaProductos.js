3210a606ccfd7a99ac55ac81ba3297664c932c36
Carolinares <caritolinareshapy@gmail.com>
2023-03-17 00:08:56 -0500

    Se agrega tabla productos

---
+++ JS/tablaProductos.js
@@ -0,0 +1,46 @@
+
+
+/*function validarTabla() //LOREE si ves esto intente hacer una validacion pero no pude
+  let formulario = document.getElementById("formulario-producto");
+
+  // Validar  campo nombre El admin debe ingresar datos correctos
+  let nombre = formulario.elements["nombre"].value;
+  if (nombre.trim() === "") { // nombre producto
+    alert("El campo 'Producto' es obligatorio.");
+    return false;
+  }
+
+  // Validar el campo imagen
+  let imagen = formulario.elements["imagen"].value;
+  if (imagen.trim() === "") {// metodo .trim elimina esoacios en blanco
+    alert("El campo 'Imagen' es obligatorio.");
+    return false;
+  }
+
+  // Validar el campo descripcion
+  let descripcion = formulario.elements["descripcion"].value;
+  if (descripcion.trim() === "") {
+    alert("El campo 'Descripción' es obligatorio.");
+    return false;
+  }
+
+  // Validar el campo porciones
+  let porciones = formulario.elements["porciones"].value;
+  if (porciones.trim() === "" || isNaN(porciones) || parseInt(porciones) < 1) {
+    alert("El campo 'Porciones' debe ser un número mayor o igual a 1.");
+    return false;
+  }
+
+  // Validar el campo precio
+  let precio = formulario.elements["precio"].value;
+  if (precio.trim() === "" || isNaN(precio) || parseInt(precio) < 0) {
+    alert("El campo 'Precio' debe ser un número mayor o igual a 0.");
+    return false;
+  }
+  // Si se han validado todos los campos*/
+
+  //Se agrega evento para que btn Agregar envie al form producto
+  document.getElementById("botonSubmit").addEventListener("click", () => {
+    console.log("Agregar productos");
+    window.location.href="../formProductos.html";
+  });
